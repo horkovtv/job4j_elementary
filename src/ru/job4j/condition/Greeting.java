b52f251f114c615d38019e643ef099358357c23c
horkov <horkovtv@gmail.com>
2021-06-01 13:52:41 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + "But I am a newbie.";
+        idea = idea + 2021;
+        System.out.println(idea);
+    }
+}
