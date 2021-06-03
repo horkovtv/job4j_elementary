e7e2ac08fc864218883f85084c0e619368f1064c
horkov <horkovtv@gmail.com>
2021-06-03 22:13:38 +0300

    6.0.1.1. Размер массива [#333582]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,8 @@ public class ArrayDefinition {
         short[] age = new short[10];
         String[] surnames = new String[10500];
         float[] prices = new float[40];
+        System.out.println("Размер массива age равен: " + age.length);
+        System.out.println("Размер массива surnames равен: " + surnames.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
