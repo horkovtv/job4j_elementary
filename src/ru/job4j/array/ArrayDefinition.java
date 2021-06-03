dc5c89c5f23cbb7c14eb601600707105ff09e098
horkov <horkovtv@gmail.com>
2021-06-03 22:20:32 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива age равен: " + age.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Камила Голубева";
+        names[1] = "Егор Семенов";
+        names[2] = "Милана Нечаева";
+        names[3] = "Александр Орлов";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
