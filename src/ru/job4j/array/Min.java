4d8800b7e853017353e532847420e4a2fc6ee57f
horkov <horkovtv@gmail.com>
2021-06-04 09:34:05 +0300

    6.5.1. Поиск минимального числа в массиве. [#179803 #164455] Исправлен обход цикла со 2 элемента массива

--- src/ru/job4j/array/Min.java
+++ src/ru/job4j/array/Min.java
@@ -4,8 +4,10 @@ public class Min {
 
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int index = 0; index < array.length; index++) {
-            min = min > array[index] ? array[index] : min;
+        for (int index = 1; index < array.length; index++) {
+            if (min > array[index]) {
+                min = array[index];
+            }
         }
         return min;
     }
