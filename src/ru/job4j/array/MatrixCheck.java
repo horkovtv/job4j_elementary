0562723d9f61c5626ca7563e651227b1063222e8
horkov <horkovtv@gmail.com>
2021-06-04 12:18:20 +0300

    6.7.1. Моно строка в матрице. [#214126 #164482] Удалена неиспользуемая переменная

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,7 +4,6 @@ public class MatrixCheck {
 
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        char monoSymbol = 'X';
         for (int i = 0; i < board[row].length; i++) {
             if (board[row][i] != 'X') {
                 result = false;
